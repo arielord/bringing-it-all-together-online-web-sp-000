71071f457d07be9c6cd0b6f0bb3130994591ec1c
Ariel <arielord@buffalo.edu>
2019-03-07 19:18:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -43,8 +43,8 @@ class Dog
     self.new_from_db(row)
   end
   
-  def find_or_create_by()
-    
+  def find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?, breed = ?")
   end
   
   def update
