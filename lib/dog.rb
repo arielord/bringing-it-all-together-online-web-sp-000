4d6290cf0121e9b5e45dab1ca717c206cab5359b
Ariel <arielord@buffalo.edu>
2019-03-07 18:37:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,7 +47,7 @@ class Dog
       SQL
       
       DB[:conn].execute(sql, self.name, self.breed)
-      @id = DB[:conn]("SELECT last_insert_rowid() FROM dogs")
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")
     end
   end
 end
